0e0fc6d86f1ac328fd6e3cb68d8c2ce349f8fb71
anna costa <45459230+annacosta2008@users.noreply.github.com>
2021-06-21 20:15:25 +0200

    Added 'Hond'

--- website/website/website/settings.py
+++ website/website/website/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'HondenTherapeut.apps.HondentherapeutConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
